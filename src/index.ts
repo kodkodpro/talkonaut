369fcbc39e64df67fa3b274239ef6ce4f8496b95
Andrew Emelianenko <emelianenko.web@gmail.com>
2023-04-12 14:14:47 -0400

    First bot response

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+import TelegramBot from "node-telegram-bot-api"
+
+if (!process.env.TELEGRAM_BOT_TOKEN) {
+  throw new Error("TELEGRAM_BOT_TOKEN is not set")
+}
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true })
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id
+
+  await bot.sendMessage(chatId, "Received your message")
+})
